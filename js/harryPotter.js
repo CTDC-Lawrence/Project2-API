4c13a781690e69d0dca2a68993e4b055cd69cf1d
Lawrence Eadie <lawrence@connectthedotsclub.com>
2021-12-09 20:55:46 -0500

    Fixed functionality with variable assignment and template literals in js for house names

--- js/harryPotter.js
+++ js/harryPotter.js
@@ -1,7 +1,8 @@
 const url = 'https://hp-api.herokuapp.com/api/characters/';
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', (event) => {
-    alert(getSelectedCheckboxValues('house'), loadCharacters);
+	event.preventDefault();
+	loadCharacters();
 });
 
 function getSelectedCheckboxValues(name) {
@@ -13,53 +14,39 @@ function getSelectedCheckboxValues(name) {
     return values;
 }
 
-function loadCharacters(event) {
-  event.preventDefault();
- // getSelectedCheckboxValues.values();
-  } fetch(url)
+function loadCharacters() {
+
+
+  fetch(url)
 	.then(function(x) {return x.json()})
 	.then(function(characterArray) {
+		let houses = getSelectedCheckboxValues('house');
+		alert(houses);
 	    let htmlTemplate = '';
 	    for (let character of characterArray) {
-	        if (getSelectedCheckboxValues('house') === 'Gryffindor') { 
-	            htmlTemplate += `
-				<div class ="Gryffindor">
-	                <h4>${character.name}</h4>                     
-	                <img src="${character.image}" alt="${character.name}" />
-				</div>
-	            `;
-	        } else if (character.house === 'Slytherin') {
-	            htmlTemplate += `
-				<div class ="Slytherin">
-	                <h4>${character.name}</h4>                     
-	                <img src="${character.image}" alt="${character.name}" />
-				</div>
-	            `;
-			} else if (character.house === 'Hufflepuff') {
-	            htmlTemplate += `
-				<div class ="Hufflepuff">
-	                <h4>${character.name}</h4>                     
-	                <img src="${character.image}" alt="${character.name}" />
-				</div>
-	            `;
-			} else if (character.house === 'Ravenclaw') {
-	            htmlTemplate += `
-				<div class ="Ravenclaw">
-	                <h4>${character.name}</h4>                     
-	                <img src="${character.image}" alt="${character.name}" />
-				</div>
-	            `;
-			} else if (character.house === 'Others') {
+			if (character.house === "") {
 				htmlTemplate += `
-				<div class ="Others">
+				<div class="Others">
 	                <h4>${character.name}</h4>                     
 	                <img src="${character.image}" alt="${character.name}" />
 				</div>
 	            `;
-			}
+			} else if ( houses.includes(character.house) ) {
+				htmlTemplate += `
+					<div class="${character.house}">
+						<h4>${character.name}</h4>                     
+						<img src="${character.image}" alt="${character.name}" />
+					</div>
+					`;	
+			} 
+
 	    }
 	    const charactersContainer = document.querySelector(".js-characters-container");
         charactersContainer.innerHTML = htmlTemplate;
 	}).catch(error => {
 		alert('Try another selection')	
 	});
+
+  } 
+  
+
